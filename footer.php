13f28e2953c36d98654dfaac5cfdffd743bae9b3
lotfar <lutfurkayes@yahoo.com>
2017-06-06 00:45:09 +0200

    Footer page

---
+++ footer.php
@@ -0,0 +1,9 @@
+<section class="fotteroption">
+	 <h2>&copy; <?php echo date("Y"); ?>  7fusion Codes</h2>
+		<p><?php echo "Please Visit: " . " http://7fusion.se/" ?></p>
+	</section>
+
+
+</div>
+</body>
+</html>
