59d6881081cbd5c4e2b4d5d326364280a798345f
lotfar <lutfurkayes@yahoo.com>
2017-06-06 00:45:43 +0200

    Master page

---
+++ master.php
@@ -0,0 +1,54 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>7fusion Codes</title>
+
+<!--Front View Template Css file Start-->
+<style type="text/css">
+	   
+    .body{font-family:<?php echo $fonts;?>}
+    .phpcoding{width:900px; margin: 0 auto; background:#ddd;}
+    .headeroption, .fotteroption{background:#444; color: #fff; text-align:center;padding:20px;}
+    .headeroption h2, .fotteroption h2 {margin: 0}
+
+    .maincontent {min-height:400px; padding:20px;}
+.error {color: #FF0000;}
+p{margin: 0}
+.tblone{width:100%;border:1px; solid #fff;margin:20px 0}
+.tblone td{padding:5px 10px; text-align: center;}
+table.tblone tr:nth-child(2n+1){background:#fff;height:30px;}
+table.tblone tr:nth-child(2n){background:#ffe;height:30px;}
+
+</style>
+
+  <link rel="stylesheet" href="css/styleshite.css">
+
+</head>
+<!--Front View Template Css file -->
+<body>
+
+
+
+<div class="phpcoding">
+	<section class="headeroption">
+		<h2><?php echo "PHP with MySQL fundamentals Traninng" ?></h2>
+  <ul class="b">
+ 
+  
+  <h3 style="color:green;" align="center">Create, Retrieve, Update, Delete (CRUD)With PHP & MySQL</h3> 
+
+
+
+ 
+
+  
+  </ul>
+	</section>
+
+	</div>
+</body>
+</html>
+
+
